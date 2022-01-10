0c55eabceb362045e388395e1c43cb1010c5407e
algo-ryth-nic <praanjjal.agnihotri@gmail.com>
2022-01-10 22:07:53 +0530

    added cmd line args for cli

--- script.py
+++ script.py
@@ -3,3 +3,14 @@ import json
 import argparse
 import pickle
 import os
+
+
+
+
+# create a cli that shows the current weather of a city 
+parser = argparse.ArgumentParser()
+# take api key
+parser.add_argument("-k", "--key", help="API key")
+parser.add_argument("-c", "--city", help="City to get the weather for", required=True)
+args = parser.parse_args()
+
