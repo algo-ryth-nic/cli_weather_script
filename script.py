2124d99ae16a67cabafc50d478b336aa1664524f
algo-ryth-nic <praanjjal.agnihotri@gmail.com>
2022-01-10 22:22:39 +0530

    restructuring of code

--- script.py
+++ script.py
@@ -7,31 +7,12 @@ import os
 # for debugging
 from pprint import pprint
 
-
 # create a cli that shows the current weather of a city 
 parser = argparse.ArgumentParser()
 # take api key
 parser.add_argument("-k", "--key", help="API key")
 parser.add_argument("-c", "--city", help="City to get the weather for", required=True)
 args = parser.parse_args()
-
-# check if the api key is given
-if args.key is None:
-    # looks for the api key in the cred.pickle file (previously saved)
-    if os.path.exists('./cred.pickle') == False:
-        print("Please provide an openweathermap's API key using -k or --key,\n \
-            view help for more info")
-        exit()
-    else:
-        # Getting credentials read pickle file 
-        with open('./cred.pickle', 'rb') as f:
-            cred = pickle.load(f)
-else:
-    # if the api key is given, save it in the cred.pickle file
-    cred = {'key': args.key}
-    with open('./cred.pickle', 'wb') as f:
-        pickle.dump(cred, f)
-
 
 
 def get_weather(city):
@@ -41,18 +22,45 @@ def get_weather(city):
     content = r.json()
     
     # debugging 
-    pprint(r.json())
+    # pprint(r.json())
     
     # print(r.status_code)
     if r.status_code == 200:
-        # request is successful, return the weather
+        # request is successful BOOYA!, return the response
         return content
     else:
         print(content['message'])
         return None
 
 
-data = get_weather(args.city)
-# create message 
-if data is not None:
-    message = "The weather in {} is {} with a temperature of {} degrees Celsius".format(args.city, data['weather'][0]['description'], data['main']['temp'])
+if __name__ == "__main__":
+
+    # check if the api key is given
+    if args.key is None:
+        # looks for the api key in the cred.pickle file (previously saved)
+        if os.path.exists('./cred.pickle') == False:
+            print("Please provide an openweathermap's API key using -k or --key,\n \
+                view help for more info")
+            exit()
+        else:
+            # Getting credentials read pickle file 
+            with open('./cred.pickle', 'rb') as f:
+                cred = pickle.load(f)
+    else:
+        # if the api key is given, save it in the cred.pickle file
+        cred = {'key': args.key}
+        with open('./cred.pickle', 'wb') as f:
+            pickle.dump(cred, f)
+
+    data = get_weather(args.city)
+    # create message 
+    if data is not None:
+        message = "The weather in {} is {} with a temperature of {} degrees Celsius".format(
+                args.city.capitalize(),
+                data['weather'][0]['description'], 
+                data['main']['temp']
+            )
+    else:
+        message = "Something went wrong with the request :("
+
+    print(message)
